3c7b8a56c08da948d8f06ae3ce8ebdd1c243caba
bluepapa32 <bluepapa32.dev@gmail.com>
2013-12-19 00:15:47 +0900

    起動時の不要なログを出力しないように修正
    Gradle のログ出力はかなり特殊で設定方法がよくわからなかったので、
    とりあえず、標準出力に出力されないようにした。

--- src/main/java/com/bluepapa32/gradle/plugins/watch/WatchTargetTaskRunner.java
+++ src/main/java/com/bluepapa32/gradle/plugins/watch/WatchTargetTaskRunner.java
@@ -1,24 +1,59 @@
 package com.bluepapa32.gradle.plugins.watch;
 
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import org.gradle.api.Project;
 import org.gradle.tooling.BuildException;
 import org.gradle.tooling.BuildLauncher;
+import org.gradle.tooling.GradleConnectionException;
 import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ProgressEvent;
+import org.gradle.tooling.ProgressListener;
 import org.gradle.tooling.ProjectConnection;
+import org.gradle.tooling.ResultHandler;
 
 public class WatchTargetTaskRunner implements AutoCloseable {
 
     private ProjectConnection connection;
 
     public WatchTargetTaskRunner(Project project) {
+
+        final PrintStream out = System.out;
+        System.setOut(new PrintStream(new OutputStream() {
+            public void write(int b)  { }
+            public void write(byte[] b)  { }
+            public void write(byte[] b, int off, int len) { }
+        }));
+
+        out.print("Starting");
+        out.flush();
+
         connection = GradleConnector.newConnector()
                 .useInstallation(project.getGradle().getGradleHomeDir())
                 .forProjectDirectory(project.getProjectDir())
                 .connect();
 
-        connection.newBuild().forTasks("watchRun").run();
+        connection
+            .newBuild()
+            .forTasks("watchRun")
+            .addProgressListener(new ProgressListener() {
+                public void statusChanged(ProgressEvent event) {
+                    out.print("..");
+                    out.flush();
+                }
+            })
+            .run(new ResultHandler<Void>() {
+                public void onComplete(Void result) {
+                    System.setOut(out);
+                    out.println(" \033[32mOK\033[39m");
+                }
+
+                @Override
+                public void onFailure(GradleConnectionException failure) {
+                }
+            });
     }
 
     public void run(List<WatchTarget> targets) {
