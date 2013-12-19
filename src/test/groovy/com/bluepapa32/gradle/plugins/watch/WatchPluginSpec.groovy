985025bba0a7a5fc1e95361e80be543018a0ceae
bluepapa32 <bluepapa32.dev@gmail.com>
2013-12-20 07:58:53 +0900

    WatchPlugin 用 Specification 追加

---
+++ src/test/groovy/com/bluepapa32/gradle/plugins/watch/WatchPluginSpec.groovy
@@ -0,0 +1,58 @@
+package com.bluepapa32.gradle.plugins.watch
+
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Rule
+import spock.lang.Specification
+
+class WatchPluginSpec extends Specification {
+
+    def project = new ProjectBuilder().build()
+
+    def "applies plugin"() {
+        when:
+        apply plugin: 'watch'
+
+        then:
+        tasks['watch'] instanceof WatchTask
+    }
+
+    def "configures watch"() {
+        setup:
+        apply plugin: 'watch'
+
+        when:
+        watch {
+            main {
+            }
+            test {
+            }
+        }
+
+        then:
+        tasks['watch'] instanceof WatchTask
+        tasks['watch'].targets.size() == 2
+
+        then:
+        tasks['watchMain'] instanceof WatchTask
+        tasks['watchMain'].targets.size() == 1
+        tasks['watchMain'].targets*.name == ['main']
+
+        then:
+        tasks['watchTest'] instanceof WatchTask
+        tasks['watchTest'].targets.size() == 1
+        tasks['watchTest'].targets*.name == ['test']
+    }
+
+//  ----------------------------------------------------------------------------
+    def methodMissing(String name, args) {
+        project."$name"(*args)
+    }
+
+    def propertyMissing(String name) {
+        project."$name"
+    }
+
+    def propertyMissing(String name, value) {
+        project."$name" = value
+    }
+}
