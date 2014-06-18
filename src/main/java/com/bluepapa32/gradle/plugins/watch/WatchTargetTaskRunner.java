78bf53fa294dfc88727db86e3b38192c669bb43b
bluepapa32 <bluepapa32.dev@gmail.com>
2014-06-19 00:18:01 +0900

    Improved startup progress logging. (fix #3)

--- src/main/java/com/bluepapa32/gradle/plugins/watch/WatchTargetTaskRunner.java
+++ src/main/java/com/bluepapa32/gradle/plugins/watch/WatchTargetTaskRunner.java
@@ -2,6 +2,8 @@ package com.bluepapa32.gradle.plugins.watch;
 
 import java.io.PrintStream;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 import org.gradle.api.Project;
 import org.gradle.api.logging.LogLevel;
@@ -14,23 +16,20 @@ import org.gradle.tooling.ProgressListener;
 import org.gradle.tooling.ProjectConnection;
 import org.gradle.tooling.ResultHandler;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
 import static org.gradle.api.logging.LogLevel.INFO;
 
 public class WatchTargetTaskRunner implements AutoCloseable {
 
     private ProjectConnection connection;
 
+    private StartupProgressMonitor monitor;
+
     public WatchTargetTaskRunner(Project project) {
 
-        final PrintStream out = System.out;
-
-        LogLevel logLevel = project.getGradle().getStartParameter().getLogLevel();
-        if (INFO.compareTo(logLevel) < 0) {
-            System.setOut(new DevNullPrintStream());
-        }
-
-        out.print("Starting");
-        out.flush();
+        monitor = new StartupProgressMonitor(project);
+        monitor.run();
 
         connection = GradleConnector.newConnector()
                 .useInstallation(project.getGradle().getGradleHomeDir())
@@ -40,20 +39,7 @@ public class WatchTargetTaskRunner implements AutoCloseable {
         connection
             .newBuild()
             .withArguments("-q")
-            .addProgressListener(new ProgressListener() {
-                public void statusChanged(ProgressEvent event) {
-                    out.print("..");
-                    out.flush();
-                }
-            })
-            .run(new ResultHandler<Void>() {
-                public void onComplete(Void result) {
-                    System.setOut(out);
-                    out.println(" OK");
-                }
-                public void onFailure(GradleConnectionException failure) {
-                }
-            });
+            .run(monitor);
     }
 
     public void run(List<WatchTarget> targets) {
@@ -94,6 +80,73 @@ public class WatchTargetTaskRunner implements AutoCloseable {
     }
 
     public void close() {
+        monitor.close();
         connection.close();
+    }
+
+    private class StartupProgressMonitor implements ResultHandler<Void> {
+
+        private static final int INTERVAL = 500;
+
+        private Project project;
+
+        private PrintStream out = System.out;
+
+        private ScheduledExecutorService timer;
+
+        public StartupProgressMonitor(Project project) {
+            this.project = project;
+            this.out = System.out;
+            this.timer = Executors.newScheduledThreadPool(1);
+        }
+
+        public void run() {
+
+            LogLevel logLevel = project.getGradle().getStartParameter().getLogLevel();
+            if (INFO.compareTo(logLevel) < 0) {
+                System.setOut(new DevNullPrintStream());
+            }
+
+            out.print("Starting...");
+            out.flush();
+
+            timer.scheduleAtFixedRate(new Runnable() {
+
+                @Override
+                public void run() {
+                    out.print("...");
+                    out.flush();
+                }
+
+            }, INTERVAL, INTERVAL, MILLISECONDS);
+        }
+
+        public void onComplete(Void result) {
+            timer.schedule(new Runnable() {
+                @Override
+                public void run() {
+                    out.println(" OK");
+                    System.setOut(out);
+                    timer.shutdownNow();
+                }
+            }, INTERVAL, MILLISECONDS);
+        }
+
+        public void onFailure(GradleConnectionException failure) {
+            timer.schedule(new Runnable() {
+                @Override
+                public void run() {
+                    out.println(" NG");
+                    System.setOut(out);
+                    timer.shutdownNow();
+                }
+            }, INTERVAL, MILLISECONDS);
+        }
+
+        public void close() {
+            if (!timer.isShutdown()) {
+                timer.shutdownNow();
+            }
+        }
     }
 }
